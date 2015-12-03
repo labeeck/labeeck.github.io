8f5250437c0098f082b92f5a895f9400a53711aa
labeeck <labeeck@gmail.com>
2015-12-03 14:56:35 +0530

    Create notification.js

---
+++ notification.js
@@ -0,0 +1,170 @@
+var isPushEnabled = false;
+
+window.addEventListener('load', function() {
+  // Check that service workers are supported, if so, progressively
+  // enhance and add push messaging support, otherwise continue without it.
+  if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.register('service-worker.js')
+    .then(initialiseState);
+  } else {
+    console.warn('Service workers aren\'t supported in this browser.');
+  }
+});
+
+// Once the service worker is registered set the initial state
+function initialiseState() {
+  // Are Notifications supported in the service worker?
+  if (!('showNotification' in ServiceWorkerRegistration.prototype)) {
+    console.warn('Notifications aren\'t supported.');
+    return;
+  }
+
+  // Check the current Notification permission.
+  // If its denied, it's a permanent block until the
+  // user changes the permission
+  if (Notification.permission === 'denied') {
+    console.warn('The user has blocked notifications.');
+    return;
+  }
+
+  // Check if push messaging is supported
+  if (!('PushManager' in window)) {
+    console.warn('Push messaging isn\'t supported.');
+    return;
+  }
+
+  // We need the service worker registration to check for a subscription
+  navigator.serviceWorker.ready.then(function(serviceWorkerRegistration) {
+    // Do we already have a push message subscription?
+    serviceWorkerRegistration.pushManager.getSubscription({ userVisibleOnly: true })  
+      .then(function(subscription) {
+        // Enable any UI which subscribes / unsubscribes from
+        // push messages.
+
+        if (!subscription) {
+          // We aren't subscribed to push, so set UI
+          // to allow the user to enable push
+          subscribe();
+          return;
+        }
+
+        // Keep your server in sync with the latest subscriptionId
+        //sendSubscriptionToServer(subscription);
+
+        // Set your UI to show they have subscribed for
+        // push messages
+
+        isPushEnabled = true;
+      })
+      .catch(function(err) {
+        console.warn('Error during getSubscription()', err);
+      });
+  });
+}
+
+function subscribe() {
+  // Disable the button so it can't be changed while
+  // we process the permission request
+
+  navigator.serviceWorker.ready.then(function(serviceWorkerRegistration) {
+    serviceWorkerRegistration.pushManager.subscribe()
+      .then(function(subscription) {
+        // The subscription was successful
+        isPushEnabled = true;
+
+        // TODO: Send the subscription.subscriptionId and
+        // subscription.endpoint to your server
+        // and save it to send a push message at a later date
+        var data = 'sender_id='+subscription.subscriptionId;
+        $.ajax({
+            type: "POST",
+            url: "adddevice",
+            data: data,
+            cache: false,
+            success: function(response){
+              window.location.reload();
+            }
+        });
+
+        //return sendSubscriptionToServer(subscription);
+      })
+      .catch(function(e) {
+        if (Notification.permission === 'denied') {
+          // The user denied the notification permission which
+          // means we failed to subscribe and the user will need
+          // to manually change the notification permission to
+          // subscribe to push messages
+          console.warn('Permission for Notifications was denied');
+        } else {
+          // A problem occurred with the subscription; common reasons
+          // include network errors, and lacking gcm_sender_id and/or
+          // gcm_user_visible_only in the manifest.
+          console.error('Unable to subscribe to push.', e);
+        }
+      });
+  });
+}
+
+function unsubscribe() {
+
+  navigator.serviceWorker.ready.then(function(serviceWorkerRegistration) {
+    // To unsubscribe from push messaging, you need get the
+    // subscription object, which you can call unsubscribe() on.
+    serviceWorkerRegistration.pushManager.getSubscription().then(
+      function(pushSubscription) {
+        // Check we have a subscription to unsubscribe
+        if (!pushSubscription) {
+          // No subscription object, so set the state
+          // to allow the user to subscribe to push
+          isPushEnabled = false;
+          return;
+        }
+
+        var subscriptionId = pushSubscription.subscriptionId;
+        // TODO: Make a request to your server to remove
+        // the subscriptionId from your data store so you
+        // don't attempt to send them push messages anymore
+
+        // We have a subscription, so call unsubscribe on it
+        pushSubscription.unsubscribe().then(function(successful) {
+          isPushEnabled = false;
+        }).catch(function(e) {
+          // We failed to unsubscribe, this can lead to
+          // an unusual state, so may be best to remove
+          // the users data from your data store and
+          // inform the user that you have done so
+
+          console.log('Unsubscription error: ', e);
+        });
+      }).catch(function(e) {
+        console.error('Error thrown while unsubscribing from push messaging.', e);
+      });
+  });
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+  if (Notification.permission !== "granted")
+    var sda="";//Notification.requestPermission();
+});
+
+function notifyMe() {
+  if (!Notification) {
+    alert('Desktop notifications not available in your browser. Try Chromium.');
+    return;
+  }
+
+  if (Notification.permission !== "granted")
+    var sdds="";//Notification.requestPermission();
+  else {
+    var notification = new Notification('Notification title', {
+      icon: 'http://cdn.sstatic.net/stackexchange/img/logos/so/so-icon.png',
+      body: "Hey there! You've been notified!",
+    });
+
+    notification.onclick = function () {
+      window.open("http://stackoverflow.com/a/13328397/1269037");
+    };
+
+  }
+
+}
